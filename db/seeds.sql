5d41b1500a838bc889da32a3afb4777c6afc8b06
Emmanuel Mosqueda Ramirez <emmanuelmosquedaramirez@Emmanuels.attlocal.net>
2018-01-18 23:23:34 -0500

    file update

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Heart Attack Buger');
+INSERT INTO burgers (burger_name) VALUES ('Hangover Buger');
+INSERT INTO burgers (burger_name) VALUES ('Good American Burger');
